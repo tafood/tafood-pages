f89a5d0477f9ab0f265c392e0483b23e290947ff
frodoeric <frodoeric@hotmail.com>
2025-03-31 17:25:43 +0100

    Update service worker and asset manifests; remove .env file

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "e22653ede92eaa0b547b48b0c8b2bc0c",
-"assets/AssetManifest.bin.json": "99a97df8255cc1642883bc1b4df3e618",
-"assets/AssetManifest.json": "e21f0de3e9ae975381dc3c047df17ba2",
+const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
+"assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "e9e008873b124384b57846766e0ab5d8",
 "assets/NOTICES": "c571b18032b5c81e610a017c34b53fa3",
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "e22653ede92eaa0b547b48b0c8b2bc0c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "1d5307c9d654c8f2dba9d8fe7ee4048b",
+"flutter_bootstrap.js": "c8807e4bbd9fd7f0c43032114b511114",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "51564bfd8286910b4c0eade23b7ebdd0",
 "/": "51564bfd8286910b4c0eade23b7ebdd0",
-"main.dart.js": "ea4cf35c2ffe448a51c6c74b46d3fe74",
+"main.dart.js": "95ead53d87e47ab35bdf0f12f5034300",
 "manifest.json": "7b011432791ad21821a78e317f5d0d66",
 "version.json": "ffadbab53d76ae80c47eb7221f625804"};
 // The application shell files that are downloaded before a service worker can
